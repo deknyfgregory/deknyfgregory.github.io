96243e0ac3a83e8653521c0f0d9211d74a93651b
De knyf Gregory <deknyfgregory@gmail.com>
2019-08-12 08:34:19 +0200

    Try resolve contact

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "https://formspree.io/iosdev.deknyfgregory@gmail.com",
+        url: "//formspree.io/iosdev.deknyfgregory@gmail.com",
         type: "POST",
         data: {
           name: name,
