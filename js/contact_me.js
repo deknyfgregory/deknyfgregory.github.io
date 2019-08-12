8565ba194ce6f10c152675528930ba362cc6ee7f
De knyf Gregory <deknyfgregory@gmail.com>
2019-08-12 08:30:35 +0200

    Try to fix contact error

--- js/contact_me.js
+++ js/contact_me.js
@@ -28,9 +28,9 @@ $(function() {
           email: email,
           message: message
         },
-        dataTye:"json",
+        dataType: "json",
         cache: false,
-        success: function() {
+        success: function(data) {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -42,12 +42,12 @@ $(function() {
           //clear all fields
           $('#contactForm').trigger("reset");
         },
-        error: function() {
+        error: function(err) {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName +", it seems that my mail server is not responding. Please try again later!" + ));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
